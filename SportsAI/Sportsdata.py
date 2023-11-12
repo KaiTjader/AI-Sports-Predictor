6193380388c7c138e339f80dd759871c6bb845bf
Kai Tjader <141417091+KaiTjader@users.noreply.github.com>
2023-11-12 22:54:23 +0000

    Updated csv file for nfl teams

--- SportsAI/Sportsdata.py
+++ SportsAI/Sportsdata.py
@@ -1,13 +1,19 @@
 import os
 import pandas as pd
+import array
 
 #Get data
-dataFrame = pd.read_csv('./SportsAI/nflData.csv')
+dataFrame = pd.read_csv('./NFLTeamScores/nflTeams.csv')
 print(dataFrame.head(10))
 
 #Reading data
 #topics = dataFrame.columns
 #iterate threw rows
+myList = []
+#for index in dataFrame['Abbreviation']:
+    #myList.append(dataFrame['Abbreviation'][index])
+print(myList)
+#print(dataFrame['Abbreviation'])
 #for index, row in dataFrame.iterrows():
-#    print(index, row)
-#dataFrame.loc[dataFrame['Position'] == "RES"]
+#    print(row)
+#dataFrame.loc[dataFrame['Abbreviation'] == "RES"]
